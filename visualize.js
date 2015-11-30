f7b9ecdb526a4dbaa49c37c800a4674737bcca03
Silvia Chyou <silvia.chyou@gmail.com>
2015-11-29 19:35:48 -0800

    heatmap

---
+++ visualize.js
@@ -0,0 +1,244 @@
+var map, heatmap;
+
+var heatmapData = [];
+var gradient = [
+    'rgba(0, 255, 255, 0)',
+    'rgba(0, 255, 255, 1)',
+    'rgba(0, 191, 255, 1)',
+    'rgba(0, 127, 255, 1)',
+    'rgba(0, 63, 255, 1)',
+    'rgba(0, 0, 255, 1)',
+    'rgba(0, 0, 223, 1)',
+    'rgba(0, 0, 191, 1)',
+    'rgba(0, 0, 159, 1)',
+    'rgba(0, 0, 127, 1)',
+    'rgba(63, 0, 91, 1)',
+    'rgba(127, 0, 63, 1)',
+    'rgba(191, 0, 31, 1)',
+    'rgba(255, 0, 0, 1)'
+]
+
+var mapstyle = [
+    {
+        "featureType": "water",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "color": "#e9e9e9"
+            },
+            {
+                "lightness": 17
+            }
+        ]
+    },
+    {
+        "featureType": "landscape",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "color": "#f5f5f5"
+            },
+            {
+                "lightness": 20
+            }
+        ]
+    },
+    {
+        "featureType": "road.highway",
+        "elementType": "geometry.fill",
+        "stylers": [
+            {
+                "color": "#ffffff"
+            },
+            {
+                "lightness": 17
+            }
+        ]
+    },
+    {
+        "featureType": "road.highway",
+        "elementType": "geometry.stroke",
+        "stylers": [
+            {
+                "color": "#ffffff"
+            },
+            {
+                "lightness": 29
+            },
+            {
+                "weight": 0.2
+            }
+        ]
+    },
+    {
+        "featureType": "road.arterial",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "color": "#ffffff"
+            },
+            {
+                "lightness": 18
+            }
+        ]
+    },
+    {
+        "featureType": "road.local",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "color": "#ffffff"
+            },
+            {
+                "lightness": 16
+            }
+        ]
+    },
+    {
+        "featureType": "poi",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "color": "#f5f5f5"
+            },
+            {
+                "lightness": 21
+            }
+        ]
+    },
+    {
+        "featureType": "poi.park",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "color": "#dedede"
+            },
+            {
+                "lightness": 21
+            }
+        ]
+    },
+    {
+        "elementType": "labels.text.stroke",
+        "stylers": [
+            {
+                "visibility": "on"
+            },
+            {
+                "color": "#ffffff"
+            },
+            {
+                "lightness": 16
+            }
+        ]
+    },
+    {
+        "elementType": "labels.text.fill",
+        "stylers": [
+            {
+                "saturation": 36
+            },
+            {
+                "color": "#333333"
+            },
+            {
+                "lightness": 40
+            }
+        ]
+    },
+    {
+        "elementType": "labels.icon",
+        "stylers": [
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
+        "featureType": "transit",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "color": "#f2f2f2"
+            },
+            {
+                "lightness": 19
+            }
+        ]
+    },
+    {
+        "featureType": "administrative",
+        "elementType": "geometry.fill",
+        "stylers": [
+            {
+                "color": "#fefefe"
+            },
+            {
+                "lightness": 20
+            }
+        ]
+    },
+    {
+        "featureType": "administrative",
+        "elementType": "geometry.stroke",
+        "stylers": [
+            {
+                "color": "#fefefe"
+            },
+            {
+                "lightness": 17
+            },
+            {
+                "weight": 1.2
+            }
+        ]
+    }
+]
+
+
+var upload = function(){
+		var answer_file = document.getElementById('answer').files[0];
+		Papa.parse(answer_file, {
+			delimiter: "^",
+			complete: function(results) {
+				console.log("Done parsing test file");
+				//console.log(results);
+				console.log(results.data.length);
+				//for(var i = 0; i< results.data.length; i++){
+				for(var i = 0; i< 30000; i++){
+					d = results.data[i];
+					//console.log(parseFloat(d[8]), parseFloat(d[7]));
+					heatmapData.push(new google.maps.LatLng(parseFloat(d[8]), parseFloat(d[7])));
+
+					/*var marker = new google.maps.Marker({       
+						position: { lat: parseFloat(d[8]), lng: parseFloat(d[7]) }, 
+						map: heatmap,  // google.maps.Map 
+						title: d[1] 
+					});*/
+				}
+				console.log("data done");
+				heatmap = new google.maps.visualization.HeatmapLayer({
+    			data: heatmapData,
+    			gradient: gradient,
+    			map: map
+  			});
+			}
+		});
+
+};
+
+var sanFrancisco = new google.maps.LatLng(37.774546, -122.433523);
+
+function initMap() {
+	var mapOptions = {
+		center: sanFrancisco,
+		zoom: 12,
+		styles: mapstyle
+	};
+	map = new google.maps.Map(
+		document.getElementById('map-canvas'), mapOptions
+	);
+
+}
+
+google.maps.event.addDomListener(window, 'load', initMap);
